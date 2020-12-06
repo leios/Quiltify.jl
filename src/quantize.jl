25d55a33e110b7ab676414ef9b3a6c345561222a
James Schloss <jrs.schloss@gmail.com>
2020-12-06 16:39:03 -0500

    fixing quantize for images with 1 in R, G, or B

--- src/quantize.jl
+++ src/quantize.jl
@@ -6,6 +6,7 @@ using DataStructures
 mutable struct ColorBox
     clr::Union{RGB, Nothing}
     children::Vector{ColorBox}
+    pixels::Vector{RGB}
     n::Int
     extents
 end
@@ -21,9 +22,9 @@ function find_extents(prev_extents, i::Int)
 end
 
 function in_box(pixel, box)
-    if pixel.r > box.extents[1][1] && pixel.r < box.extents[1][2] &&
-       pixel.g > box.extents[2][1] && pixel.g < box.extents[2][2] &&
-       pixel.b > box.extents[3][1] && pixel.b < box.extents[3][2]
+    if pixel.r > box.extents[1][1] && pixel.r <= box.extents[1][2] &&
+       pixel.g > box.extents[2][1] && pixel.g <= box.extents[2][2] &&
+       pixel.b > box.extents[3][1] && pixel.b <= box.extents[3][2]
         return true
     else
         return false
@@ -32,32 +33,47 @@ end
 
 # Small improvements could be made bu only iterating through elements of the
 # parent node
-function naive_bin!(img, box::ColorBox)
+function naive_bin!(parent, box::ColorBox)
+    println(parent.n)
     box.n = 0
 
-    for pixel in img
+    pixel_set = Array{RGB}(undef, length(parent.pixels))
+    pixel_set[:] .= RGB(0)
+
+    pixel_count = 0
+
+    for pixel in parent.pixels
+        #println(pixel)
         if in_box(pixel, box)
             if box.clr == nothing
                 box.clr = RGB(0.0)
             end
             box.n += 1
             box.clr += pixel
+            pixel_count += 1
+            pixel_set[pixel_count] = pixel
         end
     end
+
+    println(box.n, '\t', box.extents, '\t', box.clr)
+
+    if pixel_count > 0
+        box.pixels = pixel_set[1:pixel_count]
+    end 
 
     if box.clr != nothing
         box.clr /= box.n
     end
 end
 
-function divide_octree!(img, box::ColorBox, level::Int, max_level::Int)
+function divide_octree!(box::ColorBox, level::Int; max_level = 10)
     println(level)
     if level >= max_level || box.n <= 1
         return
     end
 
     # Initializing all child boxes as parent box
-    children = [ColorBox(nothing,[],0,0) for i=1:8]
+    children = [ColorBox(nothing,[],[],0,0) for i=1:8]
     box.children = children
 
     # Creating new extents and mean for each child box
@@ -66,32 +82,33 @@ function divide_octree!(img, box::ColorBox, level::Int, max_level::Int)
         children[index].extents = (find_extents(box.extents[1],i),
                                    find_extents(box.extents[2],j),
                                    find_extents(box.extents[3],k))
-        naive_bin!(img, children[index])
+        naive_bin!(box, children[index])
         println(children[index].clr)
-        divide_octree!(img, children[index], level+1, max_level)
+        divide_octree!(children[index], level+1; max_level=max_level)
     end
 
     return box
 end
 
-function make_octree(img, max_level)
+function make_octree(img; max_level = 10)
 
     # Creation of the initial octree box
-    box = ColorBox(sum(img)/(length(img)), [], length(img),
+    box = ColorBox(sum(img)/(length(img)), [], vcat(img...), length(img),
                    ((0.0, 1.0),(0.0, 1.0),(0.0, 1.0)))
 
-    divide_octree!(img, box, 1, max_level)
+    divide_octree!(box, 1; max_level = max_level)
 
 end
 
 function find_color_set!(colorset, box, color_num)
     q = PriorityQueue(Base.Order.Reverse)
-    println(typeof(box))
+    #println(typeof(box))
     enqueue!(q, box, box.n)
 
     index = 1
 
     while length(q) > 0 && index <= color_num
+        #println(q)
         temp = dequeue!(q)
         if temp.clr != nothing
             colorset[index] = temp.clr
@@ -111,9 +128,7 @@ end
 
 function quantize(img, colornum)
     img_out = copy(img)
-    max_level = ceil(Int, log2(colornum - 1)/log2(8))+1
-    println(max_level)
-    box = make_octree(img, max_level)
+    box = make_octree(img; max_level=2)
 
     colorset = [RGB(0.0, 0.0, 0.0) for i = 1:colornum]
     find_color_set!(colorset, box, colornum)
